5a55033b7d758a8bf108cae3ccdaa0b45c782e6f
thiagosullivan <thiagosullivan12@gmail.com>
2021-07-16 17:30:10 -0300

    changing img in meta tag

--- pages/index.js
+++ pages/index.js
@@ -23,18 +23,19 @@ export default function Home() {
         <meta name="description" content="Uma empresa Londrinense totalmente focada na busca por soluções tecnológicas que levam maior agilidade e confiabilidade para clientes e parceiros comerciais." />
         <link rel="icon" href="/favicon.ico" />
         <meta name="title" content="CDD CALL | Sercomtel" />
+        <meta name="author" content="Thiago Santos" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:url" content="https://cddcall.com.br/" />
         <meta property="og:title" content="CDD CALL | Sercomtel" />
         <meta property="og:description" content="Uma empresa Londrinense totalmente focada na busca por soluções tecnológicas que levam maior agilidade e confiabilidade para clientes e parceiros comerciais." />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:image" content="https://i.ibb.co/ChQB7B0/cdd-call-tag.jpg" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:url" content="https://cddcall.com.br/" />
         <meta property="twitter:title" content="CDD CALL | Sercomtel" />
         <meta property="twitter:description" content="Uma empresa Londrinense totalmente focada na busca por soluções tecnológicas que levam maior agilidade e confiabilidade para clientes e parceiros comerciais." />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="twitter:image" content="https://i.ibb.co/ChQB7B0/cdd-call-tag.jpg" />
       </Head>
       <Menu />
       <main className={styles.main}>
